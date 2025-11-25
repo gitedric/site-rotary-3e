53010d639e701bff640f5484cd2304236b8badbf
gitedric <cedric.noel@univ-lorraine.fr>
2025-11-25 18:36:25 +0100

    Ajout du guide intervenant et page dédiée

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+✗ Build failed in 507ms
+error during build:
+Could not resolve "./App.jsx" from "src/main.jsx"
+file: /home/cedric/Documents/rotary/site-rotary-3e/src/main.jsx
+    at getRollupError (file:///home/cedric/Documents/rotary/site-rotary-3e/node_modules/rollup/dist/es/shared/parseAst.js:401:41)
+    at error (file:///home/cedric/Documents/rotary/site-rotary-3e/node_modules/rollup/dist/es/shared/parseAst.js:397:42)
+    at ModuleLoader.handleInvalidResolvedId (file:///home/cedric/Documents/rotary/site-rotary-3e/node_modules/rollup/dist/es/shared/node-entry.js:21590:24)
+    at file:///home/cedric/Documents/rotary/site-rotary-3e/node_modules/rollup/dist/es/shared/node-entry.js:21550:26
+
